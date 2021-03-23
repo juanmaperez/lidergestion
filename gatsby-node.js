bb75df89b8657bf6e890341927c3107a37c213b8
juanmaperez <juanmaperezvar@gmail.com>
2021-03-23 09:37:56 +0000

    create post and page templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,70 @@
-exports.onCreatePage = ({ page, actions }) => {
-  const { createPage } = actions;
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const resultPages = await graphql(`
+    {
+      allWpPage {
+        nodes {
+          id
+          slug
+        }
+      }
+    }
+  `)
 
-  if (page.path.match(/sign|reset/)) {
-    page.context.layout = "mini";
-    createPage(page);
-  } else if (page.path.match(/coming/)) {
-    page.context.layout = "coming";
-    createPage(page);
+  if (resultPages.errors) {
+    reporter.error("There was an error fetching pages", resultPages.errors)
   }
+
+  const { allWpPage } = resultPages.data
+
+  // Define the template to use
+  const template = require.resolve(`./src/templates/page.js`)
+
+  console.log(allWpPage)
+
+  if (allWpPage.nodes.length) {
+    allWpPage.nodes.map(page => {
+      actions.createPage({
+        // It's best practice to use the uri field from WPGraphQL nodes when
+        // building
+        path:  page.slug === 'home' ? `/` : `/${slug}` ,
+        component: template,
+        context: page,
+      })
+    })
+  }
+
+  const resultPost = await graphql(`
+    {
+      allWpPost {
+        nodes {
+          id
+          slug
+        }
+      }
+    }
+  `)
   
-};
+  if (resultPost.errors) {
+    reporter.error("There was an error fetching posts", resultPost.errors)
+  }
+
+  const { allWpPost } = resultPost.data
+
+  // Define the template to use
+  const postTemplate = require.resolve(`./src/templates/post.js`)
+
+  if (allWpPost.nodes.length) {
+    allWpPost.nodes.map(post => {
+      actions.createPage({
+        // It's best practice to use the uri field from WPGraphQL nodes when
+        // building
+        path:  `/blog/${post.slug}` ,
+        component: postTemplate,
+        context: post,
+      })
+    })
+  }
+}
+
+
+
---
+++ src/templates/post.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { graphql } from "gatsby"
+
+const PostTemplate = ({ data }) => {
+	const { wpPost } = data;
+	const { title, content, date, tags, featuredImage } = wpPost
+	const imageUrl = featuredImage ? featuredImage.node.sourceUrl : null
+	console.log(imageUrl)
+	const tagNames = tags ? tags.nodes.map(tag => tag.name) : null;
+
+	return (
+		<main className="pt-15 pt-lg-20 pb-13 pb-md-19 pb-lg-27">
+			{ imageUrl && (
+				<div
+					className="bg-img-1 bg-images mx-lg-13 mx-xl-15 bg-gradient-2"
+					style={{ backgroundImage: `url(${imageUrl})`, minHeight: '50vh', backgroundPosition: 'center center' }}
+				>
+						<div className="container">
+						<div className="row">
+						</div>
+					</div>
+				</div>
+			)}
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-xl-11">
+						{/* job-details-content */}
+						<div className="text-center pb-0">
+							<div className="mb-5 d-inline-flex align-items-center flex-wrap">
+								<span className="d-block d-inline-flex align-items-center mr-sm-6 text-bali-gray">
+									{ date }
+								</span>
+							</div>
+							<h2 className="font-size-11 font-weight-bold text-center mb-lg-15 mb-0">
+								{ title }
+							</h2>
+							{	tagNames && (
+								<div className="d-inline-flex align-items-center flex-wrap">
+									{ tagNames.map(tag => (
+										<p className="bg-dark-green-op1 font-size-3 font-weight-medium badge mb-0 py-2 px-4 ml-8">
+											<span className="text-dark-green">{ tag }</span>
+										</p>
+									))}
+								</div>
+							)}
+						</div>
+						<div className="col-xl-9 col-lg-10 px-xl-0 px-lg-6 px-md-0 px-6 pr-0">
+							<div className="pt-lg-0 pt-10 pl-lg-10 px-xl-15 font-size-5 heading-default-color" dangerouslySetInnerHTML={{__html: content }}>
+							
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+	)
+}
+
+export const query = graphql`
+  query($id: String) {
+    wpPost(id: { eq: $id}){
+			title
+			uri
+			excerpt
+			content
+			date
+			tags {
+				nodes {
+					name
+				}
+			}
+			categories {
+				nodes {
+					name
+				}
+			}
+			featuredImage {
+				node {
+					sourceUrl
+				}
+			}
+		}
+	}
+`
+
+
+export default PostTemplate
