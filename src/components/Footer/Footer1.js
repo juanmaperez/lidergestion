210d7ba3fb5a4e45c33283a43899c70c020c6206
juanmaperez <juanmaperezvar@gmail.com>
2021-04-02 16:52:26 +0100

    footer menu

--- src/components/Footer/Footer1.js
+++ src/components/Footer/Footer1.js
@@ -1,9 +1,39 @@
 import React from "react";
-import { Link } from "gatsby";
-
+import { graphql, Link, useStaticQuery } from "gatsby";
 import imgLB from "../../assets/image/logo_lidergestion-200.svg";
 
+
+const query = graphql`
+  query {
+    wpMenu(id: {eq: "dGVybToxNzE="}) {
+      id
+      name
+      menuItems {
+        nodes {
+          parentId
+          url
+          label
+          childItems {
+            nodes {
+              url
+              label
+              childItems {
+                nodes {
+                  label
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 const Footer = ({ className, ...rest }) => {
+  const { wpMenu } = useStaticQuery(query)
+
   return (
     <>
       <div className={`pb-13 pb-lg-10 ${className}`} {...rest}>
@@ -32,114 +62,68 @@ const Footer = ({ className, ...rest }) => {
             </div>
             <div className="col-lg-8 col-md-7 offset-lg-1">
               <div className="row">
-                {/* Single Widgets */}
-                <div className="col-md-4 col-xs-6">
-                  <div className="mb-10 mb-lg-0">
-                    <h4 className="font-size-6 font-weight-medium mb-5">
-                      Dónde encontrarnos
-                    </h4>
-                    <ul className="list-unstyled">
-                      <li className="mb-7">
-                        <a
-                          target="blank"
-                          href="https://www.google.com/maps/place/CORREDURIA+DE+SEGUROS+LIDERGESTION/@37.3995173,-6.0373372,17z/data=!4m12!1m6!3m5!1s0xd126d1e81c7783d:0x6dc61e8930284ebf!2sCORREDURIA+DE+SEGUROS+LIDERGESTION!8m2!3d37.3995173!4d-6.0351485!3m4!1s0xd126d1e81c7783d:0x6dc61e8930284ebf!8m2!3d37.3995173!4d-6.0351485"
-                          className="font-size-5 text-stone gr-hover-text-primary"
-                        >
-                          Plaza Santa Ana <br/>
-                          Nº 11 LOCAL 5 <br/> 
-                          41900 Camas.Sevilla
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                {/* End Single Widgets */}
-                {/* Single Widgets */}
-                <div className="col-md-4 col-xs-6">
-                  <div className="mb-10 mb-lg-0">
-                    <h4 className="font-size-6 font-weight-medium mb-5">
-                      Cómo contactar
-                    </h4>
-                    <ul className="list-unstyled">
-                      <li className="mb-2">
-                        <a
-                          href="tel:955981949"
-                          className="font-size-5 text-stone gr-hover-text-primary"
-                        >
-                          95 598 19 49                          
-                        </a>
-                      </li>
-                      <li className="mb-2">
-                        <a
-                          href="tel:625036750"
-                          className="font-size-5 text-stone gr-hover-text-primary"
-                        >
-                          625 036 750
-                        </a>
-                      </li>
-                      <li className="mb-2">
-                        <a
-                          href="tel:655927366"
-                          className="font-size-5 text-stone gr-hover-text-primary"
-                        >
-                          655 927 366
-                        </a>
-                      </li>
-                      <li className="mb-2">
-                        <a
-                          href="mailto:lidergestion10@hotmail.com"
-                          className="font-size-5 text-stone gr-hover-text-primary"
-                        >
-                          lidergestion10@hotmail.com
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                {/* End Single Widgets */}
-                {/* Single Widgets */}
-                <div className="col-md-4 col-xs-6">
-                  <div className="mb-10 mb-lg-0">
-                    <h4 className="font-size-6 font-weight-medium mb-5">
-                      Páginas de interés
-                    </h4>
-                    <ul className="list-unstyled">
-                      <li className="mb-2">
-                        <Link
-                          to="/#"
-                          className="font-size-5 text-stone gr-hover-text-primary"
-                        >
-                          Contacto
-                        </Link>
-                      </li>
-                      <li className="mb-2">
-                        <Link
-                          to="/#"
-                          className="font-size-5 text-stone gr-hover-text-primary"
-                        >
-                          Asistencia en carretera
-                        </Link>
-                      </li>
-                      <li className="mb-2">
-                        <Link
-                          to="/#"
-                          className="font-size-5 text-stone gr-hover-text-primary"
-                        >
-                          Preguntas frequentes
-                        </Link>
-                      </li>
-                      <li className="mb-2">
-                        <Link
-                          to="/#"
-                          className="font-size-5 text-stone gr-hover-text-primary"
-                        >
-                          Blog
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                {/* End Single Widgets */}
+              { wpMenu && wpMenu.menuItems.nodes.filter(({ parentId}) => parentId === null).map(
+                ({ label, url, childItems, parentId, ...rest }, index) => {
+                  const isExternal = !url.startsWith('/')
+                  const hasSubItems = Array.isArray(childItems.nodes) && childItems.nodes.length > 0;
+                  return (
+                    <React.Fragment key={label + index}>
+                      { hasSubItems ? (
+                        <div className="col-md-4 col-xs-6">
+                          <div className="mb-10 mb-lg-0">
+                            <h4 className="font-size-6 font-weight-medium mb-5">
+                              {label}
+                            </h4>
+                            <ul className="list-unstyled">
+                              { childItems.nodes.map(
+                                (itemInner, indexInnerMost) => (
+                                  <li className="mb-2"
+                                    key={itemInner.label + indexInnerMost}
+                                  >
+                                    { !itemInner.url.startsWith('/') ? (
+                                      <a
+                                        href={`${itemInner.url}`}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                      >
+                                        {itemInner.label}
+                                      </a>
+                                    ) : (
+                                      <Link to={`${itemInner.url}`}>
+                                        {itemInner.label}
+                                      </Link>
+                                    )}
+                                  </li>
+                                )
+                              )}
+                            </ul>
+
+                          </div>
+                        </div>
+                      ):(
+                        <ul className="list-unstyled">
+                          <li className="mb-2"
+                            key={label}
+                          >
+                            { isExternal ? (
+                              <a
+                                href={`${ url}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                { label}
+                              </a>
+                            ) : (
+                              <Link to={`${ url}`}>
+                                {label}
+                              </Link>
+                            )}
+                          </li>
+                        </ul>
+                      )}
+                    </React.Fragment>
+                  )}
+              )}                
               </div>
             </div>
           </div>
